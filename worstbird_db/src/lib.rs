3a7391e32ca7cb574a4cc1bc9367e228a5bd6350
Sebastian Heusinger <heusings@googlemail.com>
2021-11-13 21:14:09 +0100

    added twitter functionality

--- worstbird_db/src/lib.rs
+++ worstbird_db/src/lib.rs
@@ -3,3 +3,66 @@ pub mod schema;
 
 #[macro_use]
 extern crate diesel;
+extern crate dotenv;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub fn establish_connection_env() -> Result<PgConnection> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("No DATABASE_URL");
+    Ok(PgConnection::establish(&database_url)?)
+}
+
+pub fn get_birds_year(sel_year: i32, con: &PgConnection) -> Result<Vec<(models::Bird, i32)>> {
+    use crate::schema::bird::dsl::*;
+    use crate::schema::worstbird_year::dsl::*;
+    Ok(worstbird_year
+        .filter(year.eq(sel_year))
+        .inner_join(bird)
+        .select(((id, name, description, assetid, url, width, height), votes))
+        .load(con)?)
+}
+
+pub fn get_birds_month(
+    sel_month: i32,
+    sel_year: i32,
+    con: &PgConnection,
+) -> Result<Vec<(models::Bird, i32)>> {
+    use crate::schema::bird::dsl::*;
+    use crate::schema::worstbird_month::dsl::*;
+    Ok(worstbird_month
+        .filter(month.eq(sel_month))
+        .filter(year.eq(sel_year))
+        .inner_join(bird)
+        .select(((id, name, description, assetid, url, width, height), votes))
+        .load(con)?)
+}
+
+pub fn get_worstbird_year(sel_year: i32, con: &PgConnection) -> Result<Vec<(models::Bird, i32)>> {
+    let birds_year = get_birds_year(sel_year, con)?;
+    let max_votes = birds_year.iter().map(|e| e.1).max().unwrap_or(0);
+
+    Ok(birds_year
+        .into_iter()
+        .filter(|e| e.1 == max_votes)
+        .collect::<Vec<(models::Bird, i32)>>())
+}
+
+pub fn get_worstbird_month(
+    sel_month: i32,
+    sel_year: i32,
+    con: &PgConnection,
+) -> Result<Vec<models::Bird>> {
+    let results = get_birds_month(sel_month, sel_year, con)?;
+    let max_votes = results.iter().map(|e| e.1).max().unwrap_or(0);
+    Ok(results
+        .iter()
+        .filter(|e| e.1 == max_votes)
+        .map(|e| e.0.clone())
+        .collect::<Vec<models::Bird>>())
+}
--- worstbird_twitter/src/main.rs
+++ worstbird_twitter/src/main.rs
@@ -1,19 +1,14 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use]
-extern crate rocket;
-
 extern crate dotenv;
 use dotenv::dotenv;
 use std::env;
 use twitter_api::{SigningKey, TwitterApi};
 
-use rocket::form::FromForm;
-use rocket::routes;
-use rocket::serde::json::Json;
-use rocket::State;
+use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::time::Duration;
+use worstbird_db::*;
 
-#[derive(FromForm, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct TweetMSG {
     size: i32,
     text: String,
@@ -24,41 +19,67 @@ struct ResponseJson {
     response_code: i32,
     response_msg: String,
 }
-
-#[post("/tweet", format = "json", data = "<twitter_msg>")]
 async fn tweet(
-    twitter: &State<TwitterApi>,
-    twitter_msg: Json<TweetMSG>,
-) -> Result<Json<ResponseJson>, Json<ResponseJson>> {
+    twitter: &TwitterApi,
+    twitter_msg: TweetMSG,
+) -> Result<(), Box<dyn std::error::Error>> {
     if twitter_msg.size <= 280
         && twitter_msg.size > 0
         && twitter_msg.text.len() < 280
         && twitter_msg.text.len() > 0
     {
-        let response = twitter.tweet(&twitter_msg.text);
-        match response.await {
-            Ok(_) => Ok(ResponseJson {
-                response_code: 0,
-                response_msg: twitter_msg.text.clone(),
-            }
-            .into()),
-            Err(e) => Err(ResponseJson {
-                response_code: 1,
-                response_msg: e.to_string(),
-            }
-            .into()),
-        }
+        twitter.tweet(&twitter_msg.text).await?;
+        Ok(())
     } else {
-        Err(ResponseJson {
-            response_code: 2,
-            response_msg: "Message to big or to small to Tweet".to_string(),
-        }
-        .into())
+        return Err("Message to long".into());
     }
 }
 
-#[launch]
-fn rocket() -> _ {
+fn calc_time_to_end_of_month() -> Duration {
+    let now = Utc::now();
+    let new_date;
+    if now.month() == 12 {
+        new_date = Utc.ymd(now.year() + 1, 1, 1).and_hms(0, 5, 0);
+    } else {
+        new_date = Utc.ymd(now.year(), now.month() + 1, 1).and_hms(0, 5, 0);
+    }
+
+    let duration = new_date.signed_duration_since(now);
+    duration.to_std().unwrap()
+}
+
+async fn send_post(twitter_api: &TwitterApi) -> Result<(), Box<dyn std::error::Error>> {
+    let now = Utc::now();
+    let month = now.month() as i32;
+    // calculate last month
+    let prev_month = get_previous_month(month);
+    let prev_year = get_previous_year_if_necessary(month, now.year() as i32);
+    let con = establish_connection_env()?;
+
+    let birds = get_worstbird_month(prev_month, prev_year, &con)?;
+    for bird in birds.into_iter() {
+        let text = format!(
+            "The worstbird of the month: {}\n{} {}\n{}",
+            bird.name,
+            "@daspodcastufo".to_string(),
+            "worstbird.eu".to_string(),
+            bird.url,
+        );
+
+        tweet(
+            &twitter_api,
+            TweetMSG {
+                size: text.len() as i32,
+                text,
+            },
+        )
+        .await?;
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let twitter_api = TwitterApi::new(
         &env::var("CONSUMER_KEY").unwrap(),
@@ -68,8 +89,55 @@ fn rocket() -> _ {
             &env::var("TOKEN_SECRET").unwrap(),
         ),
     );
+    loop {
+        let wait_time = calc_time_to_end_of_month();
+        std::thread::sleep(wait_time);
+        send_post(&twitter_api).await?;
+    }
+}
 
-    rocket::build()
-        .mount("/", routes![tweet])
-        .manage(twitter_api)
+// takes the original month and returns the previous year if the month is january
+fn get_previous_year_if_necessary(month: i32, year: i32) -> i32 {
+    if month == 12 {
+        year - 1
+    } else {
+        year
+    }
+}
+
+fn get_previous_month(month: i32) -> i32 {
+    let prev_month = ((month - 1) % 12) + ((month - 12) / 11) * -12;
+    prev_month
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test1() {
+        for i in 1..12 {
+            println!("{}|{}", i, get_previous_month(i));
+        }
+        assert!(true);
+    }
+    macro_rules! aw {
+        ($e:expr) => {
+            tokio_test::block_on($e)
+        };
+    }
+
+    #[test]
+    fn test_tweet() {
+        dotenv().ok();
+        let twitter_api = TwitterApi::new(
+            &env::var("CONSUMER_KEY").unwrap(),
+            &env::var("TOKEN").unwrap(),
+            SigningKey::new(
+                &env::var("CONSUMER_SECRET").unwrap(),
+                &env::var("TOKEN_SECRET").unwrap(),
+            ),
+        );
+
+        aw!(send_post(&twitter_api));
+    }
 }
